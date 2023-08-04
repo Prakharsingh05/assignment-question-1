4b5817d116a558c9605c17afe297adcf3ccc8fe2
Prakhar Singh <prakharsingh285@gmail.com>
2023-08-04 22:15:38 +0530

    Updated Dashboard component to show number of orders in the secondary title.

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -30,43 +30,45 @@ const Dashboard = () => {
 	const handleClick = (itemId) => {
 		const selectedData = data.find((item) => item["&key"] === itemId)
 
-		setRowKey(selectedData["&key"])
+		setNewKey(selectedData["&key"])
 		setSelectedOrderDetails(selectedData.executionDetails)
 		setSelectedOrderTimeStamps(selectedData.timestamps)
 	};	
 
-  
+
   return (
-    <div>
-      <div className={styles.header}>
-        <HeaderTitle primaryTitle="Orders" secondaryTitle="6 orders" />
-        <div className={styles.actionBox}>
-          <Search
-            value={searchText}
-            onChange={(e) => setSearchText(e.target.value)}
-          />
-          <Dropdown
-            options={["GBP", "USD", "JPY", "EUR"]}
-            onChange={(e) => setCurrency(e.target.value)}
-            selectedItem={currency}
-          />
-        </div>
-      </div>
-      <div className={styles.content}>
-        <div className={styles.section}>
-          <Card
-            cardData={selectedOrderDetails}
-            title="Selected Order Details"
-          />
-          <Card
-            cardData={selectedOrderTimeStamps}
-            title="Selected Order Timestamps"
-          />
-        </div>
-        <List rows={mockData.results} />
-      </div>
-    </div>
-  );
+		<div>
+			<div className={styles.header}>
+				<HeaderTitle primaryTitle="Orders" secondaryTitle={`${mockData.results.length} orders`} />
+				<div className={styles.actionBox}>
+					<Search
+						value={searchText}
+						onChange={(e) => setSearchText(e.target.value)}
+					/>
+					<Dropdown
+						options={["GBP", "USD", "JPY", "EUR"]}
+						onChange={(e) => setCurrency(e.target.value)}
+						selectedItem={currency}
+					/>
+				</div>
+			</div>
+			<div className={styles.content}>
+				<div className={styles.section}>
+					<Card
+						cardData={selectedOrderDetails}
+						title="Selected Order Details"
+						newKey={newKey}
+					/>
+					<Card
+						cardData={selectedOrderTimeStamps}
+						title="Selected Order Timestamps"
+						newKey={newKey}
+					/>
+				</div>
+				<List rows={mockData.results} orders={timestamps.results} currency={currency} search={searchText} onRowClick={handleClick} />
+			</div>
+		</div>
+	);
 };
 
 export default Dashboard;
