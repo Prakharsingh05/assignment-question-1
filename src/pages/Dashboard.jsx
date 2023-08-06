5cf740a6075e57b3e36e32cae558d16fe9efd95b
Prakhar Singh <prakharsingh285@gmail.com>
2023-08-06 18:25:20 +0530

    Updated Dashboard component to use useState hook.

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,16 +1,10 @@
-import { useState } from "react";
-
-// Data
+import React, { useState } from "react";
 import mockData from "../assets/data.json";
 import timestamps from "../assets/timeStamps.json";
-
-// Components
 import Dropdown from "../component/dropdown/Dropdown";
 import HeaderTitle from "../component/header-title/HeaderTitle";
 import Search from "../component/search/Search";
 import List from "../component/list/List";
-
-// Styles
 import styles from "./Dashboard.module.css";
 import Card from "../component/card/Card";
 
@@ -21,54 +15,64 @@ const Dashboard = () => {
   const [selectedOrderDetails, setSelectedOrderDetails] = useState({});
   const [selectedOrderTimeStamps, setSelectedOrderTimeStamps] = useState({});
 
-    const rows = mockData.results
-    const orders = timestamps.results
+  const rows = mockData.results;
+  const orders = timestamps.results;
 
-    const data = rows.map((rows, index) => ({ ...rows, ...orders[index] }))
+  const data = rows.map((rows, index) => ({
+    ...rows,
+    ...orders[index],
+  }));
 
-    
-	const handleClick = (itemId) => {
-		const selectedData = data.find((item) => item["&key"] === itemId)
+  const handleClick = (itemId) => {
+    const selectedData = data.find((item) => item["&key"] === itemId);
 
-		setNewKey(selectedData["&key"])
-		setSelectedOrderDetails(selectedData.executionDetails)
-		setSelectedOrderTimeStamps(selectedData.timestamps)
-	};	
-
+    setNewKey(selectedData["&key"]);
+    setSelectedOrderDetails(selectedData.executionDetails);
+    setSelectedOrderTimeStamps(selectedData.timestamps);
+  };
 
   return (
-		<div>
-			<div className={styles.header}>
-				<HeaderTitle primaryTitle="Orders" secondaryTitle={`${mockData.results.length} orders`} />
-				<div className={styles.actionBox}>
-					<Search
-						value={searchText}
-						onChange={(e) => setSearchText(e.target.value)}
-					/>
-					<Dropdown
-						options={["GBP", "USD", "JPY", "EUR"]}
-						onChange={(e) => setCurrency(e.target.value)}
-						selectedItem={currency}
-					/>
-				</div>
-			</div>
-			<div className={styles.content}>
-				<div className={styles.section}>
-					<Card
-						cardData={selectedOrderDetails}
-						title="Selected Order Details"
-						newKey={newKey}
-					/>
-					<Card
-						cardData={selectedOrderTimeStamps}
-						title="Selected Order Timestamps"
-						newKey={newKey}
-					/>
-				</div>
-				<List rows={mockData.results} orders={timestamps.results} currency={currency} search={searchText} onRowClick={handleClick} />
-			</div>
-		</div>
-	);
+    <div>
+      <div className={styles.header}>
+        <HeaderTitle
+          primaryTitle="Orders"
+          secondaryTitle={`${data.length} orders`}
+        />
+        <div className={styles.actionBox}>
+          <Search
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <Dropdown
+            options={["GBP", "USD", "JPY", "EUR"]}
+            onChange={(e) => setCurrency(e.target.value)}
+            selectedItem={currency}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.section}>
+          <Card
+            cardData={selectedOrderDetails}
+            title="Selected Order Details"
+            newKey={newKey}
+          />
+          <Card
+            cardData={selectedOrderTimeStamps}
+            title="Selected Order Timestamps"
+            newKey={newKey}
+          />
+        </div>
+        <List
+          rows={mockData.results}
+          orders={timestamps.results}
+          currency={currency}
+          search={searchText}
+          onRowClick={handleClick}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
